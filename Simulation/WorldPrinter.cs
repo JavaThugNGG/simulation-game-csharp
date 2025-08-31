a3c84fc5d8f6f5e029f76824436a24bc377860cb
JavaThugNGG <ildarjava@mail.ru>
2025-08-31 21:11:54 +0300

    feat: add WorldPrinter class

---
+++ Simulation/WorldPrinter.cs
@@ -0,0 +1,30 @@
+﻿namespace Simulation
+{
+    internal class WorldPrinter
+    {
+        internal void Print(IDictionary<Coordinates, Entity> inputDictionary)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < SimulationManager.WorldRows; i++)
+            {
+                for (int j = 0; j < SimulationManager.WorldColumns; j++)
+                {
+                    Coordinates coordinate = new Coordinates(i, j);
+
+                    if (inputDictionary.TryGetValue(coordinate, out Entity? entity))
+                    {
+                        Console.Write(entity);
+                    }
+                    else
+                    {
+                        Console.Write("⬛");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
