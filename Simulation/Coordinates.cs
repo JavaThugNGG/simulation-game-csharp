3f5060ae6a0fb366dd2a69316f43ae23c0890fa5
JavaThugNGG <ildarjava@mail.ru>
2025-08-31 19:19:04 +0300

    feat: add Coordinates class for representing 2D points

---
+++ Simulation/Coordinates.cs
@@ -0,0 +1,9 @@
+﻿namespace Simulation
+{
+    internal record Coordinates(int Row, int Column) : IComparable<Coordinates>
+    {
+        public override string ToString() => $"{Row} {Column}";
+
+        public int CompareTo(Coordinates other) => (Row, Column).CompareTo((other.Row, other.Column));
+    }
+}
