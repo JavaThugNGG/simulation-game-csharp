e6d07f7aaa0915da820028a82ff51fcc496c6aed
JavaThugNGG <ildarjava@mail.ru>
2025-08-31 19:21:05 +0300

    feat: add Creature class extending Entity class

---
+++ Simulation/Creature.cs
@@ -0,0 +1,23 @@
+﻿namespace Simulation
+{
+    internal abstract class Creature : Entity
+    {
+        internal delegate void MoveHandler(object sender, Coordinates oldCoordinates, Coordinates newCoordinates);
+        internal event MoveHandler Moved;
+
+        internal Creature(Coordinates coordinates, string figure)//предупреждение уйдет, когда на событие подпишутся
+            : base(coordinates, figure) { }
+
+        internal void MakeMove(Coordinates newCoordinates)
+        {
+            Coordinates oldCoordinates = Coordinates;
+            Coordinates = newCoordinates;
+            OnMove(oldCoordinates, newCoordinates);
+        }
+
+        private void OnMove(Coordinates oldCoordinates, Coordinates newCoordinates)
+        {
+            Moved?.Invoke(this, oldCoordinates, newCoordinates);
+        }
+    }
+}
