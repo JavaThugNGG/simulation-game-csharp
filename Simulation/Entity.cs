5a691dfc3db7ab4b90bf466e0dba92aadfba5e1c
JavaThugNGG <ildarjava@mail.ru>
2025-08-31 19:20:03 +0300

    feat: add base Entity class for inherit from

---
+++ Simulation/Entity.cs
@@ -0,0 +1,17 @@
+﻿namespace Simulation
+{
+    internal abstract class Entity
+    {
+        internal Entity(Coordinates coordinates, string figure)
+        {
+            Coordinates = coordinates;
+            Figure = figure;
+        }
+
+        protected Coordinates Coordinates { get; set; }
+
+        protected string Figure { get; init; }
+
+        public override string ToString() => Figure;
+    }
+}
