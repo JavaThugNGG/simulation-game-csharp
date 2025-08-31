604cdd68683476156eea0f3b76131341fd8327fd
JavaThugNGG <ildarjava@mail.ru>
2025-08-31 20:18:58 +0300

    feat: add SimulationManager class with constants for 2D world size

---
+++ Simulation/SimulationManager.cs
@@ -0,0 +1,8 @@
+﻿namespace Simulation
+{
+    internal class SimulationManager
+    {
+        public static readonly int WorldRows = 10;
+        public static readonly int WorldColumns = 20;
+    }
+}
