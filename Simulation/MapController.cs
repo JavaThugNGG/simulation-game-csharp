4d155d6c26392b35b211cb409dcb53a3a0f94b7e
JavaThugNGG <ildarjava@mail.ru>
2025-09-01 13:27:17 +0300

    feat: add MapController class

---
+++ Simulation/MapController.cs
@@ -0,0 +1,32 @@
+﻿namespace Simulation
+{
+    internal class MapController
+    {
+        private readonly IDictionary<Coordinates, Entity> _map;
+
+        internal MapController(IDictionary<Coordinates, Entity> map, List<Entity> generatedEntities)
+        {
+            _map = map;
+            FillMap(generatedEntities);
+        }
+
+        private void FillMap(List<Entity> generatedEntities)
+        {
+            foreach (Entity entity in generatedEntities)
+            {
+                Coordinates entityCoordinates = entity.Coordinates;
+                _map[entityCoordinates] = entity;
+
+                if (entity is Creature creature)
+                {
+                    creature.Moved += OnCreatureMoved;
+                }
+            }
+        }
+        private void OnCreatureMoved(object sender, Coordinates oldCoordinates, Coordinates newCoordinates)
+        {
+            _map[newCoordinates] = (Entity)sender;
+            _map.Remove(oldCoordinates);
+        }
+    }
+}
